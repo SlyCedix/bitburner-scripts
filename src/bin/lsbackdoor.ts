cd6bab1ccd3a9243b730ece3fe303e779f623d3f
Cyn <SlyCedix@gmail.com>
2021-12-23 18:37:38 -0500

    fix lsb and lsc printing wrong

--- src/bin/lsbackdoor.ts
+++ src/bin/lsbackdoor.ts
@@ -5,6 +5,6 @@ import { getServersWithoutBackdoor } from '/lib/helpers.js'
 export async function main(ns : NS) : Promise<void>{
     const servers = await getServersWithoutBackdoor(ns)
 
-    if(servers.length > 0 ) ns.tprint(`INFO: These servers need to be backdoored ${servers.values}`)
+    if(servers.length > 0 ) ns.tprint(`INFO: These servers need to be backdoored ${servers}`)
     else ns.tprint('INFO: All rooted servers are backdoored')
 }
--- src/bin/lscontracts.ts
+++ src/bin/lscontracts.ts
@@ -5,6 +5,6 @@ import { getServersWithContracts } from '/lib/helpers.js'
 export async function main(ns : NS) : Promise<void>{
     const servers = await getServersWithContracts(ns)
 
-    if(servers.length > 0 ) ns.tprint(`INFO: These servers have contracts ${servers.values}`)
+    if(servers.length > 0 ) ns.tprint(`INFO: These servers have contracts ${servers.toString()}`)
     else ns.tprint('INFO: There are no servers with contracts')
 }
