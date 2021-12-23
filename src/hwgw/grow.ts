42d4e71350ec068c9c03be576fc8c4f08322aa8e
Cyn <SlyCedix@gmail.com>
2021-12-23 18:37:50 -0500

    remove loops from hack scripts

--- src/hwgw/grow.ts
+++ src/hwgw/grow.ts
@@ -1,8 +1,5 @@
 import { NS } from '../../NetscriptDefinitions'
 
 export async function main(ns : NS) : Promise<void> {
-    while (true) {
-        await ns.sleep(Number(ns.args[1]))
-        await ns.grow(ns.args[0].toString())
-    }
+    await ns.grow(ns.args[0].toString())
 }
--- src/hwgw/weaken.ts
+++ src/hwgw/weaken.ts
@@ -1,8 +1,5 @@
 import { NS } from '../../NetscriptDefinitions'
 
 export async function main(ns : NS) : Promise<void> {
-    while (true) {
-        await ns.sleep(Number(ns.args[1]))
-        await ns.weaken(ns.args[0].toString())
-    }
+    await ns.weaken(ns.args[0].toString())
 }
