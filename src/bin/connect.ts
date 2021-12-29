3cb4061dac3e633555aeb28885450a965b593234
Cyn <SlyCedix@gmail.com>
2021-12-29 13:39:57 -0500

    Fix whitespace

--- src/bin/connect.ts
+++ src/bin/connect.ts
@@ -3,25 +3,25 @@ import { ServerData } from '../../types'
 import { findServer, runTerminalCommand } from '/lib/helpers.js'
 
 export async function main(ns: NS): Promise<void> {
-	if (ns.args.length != 1) {
-		ns.tprintf('ERROR: Incorrect usage of connect command. Usage: connect [ip/hostname]')
-	} else {
-		const connections = await findServer(ns, ns.args[0] as string)
-		let connectString = 'home; '
+  if (ns.args.length != 1) {
+    ns.tprintf('ERROR: Incorrect usage of connect command. Usage: connect [ip/hostname]')
+  } else {
+    const connections = await findServer(ns, ns.args[0] as string)
+    let connectString = 'home; '
 
-		for (const connection of connections) {
-			if (connection != 'home') connectString += `connect ${connection}; `
-		}
+    for (const connection of connections) {
+      if (connection != 'home') connectString += `connect ${connection}; `
+    }
 
-		if (connectString.length > 0) {
-			runTerminalCommand(connectString)
-		} else {
-			ns.tprintf(`ERROR: Host ${ns.args[0]} not found.`)
-		}
-	}
+    if (connectString.length > 0) {
+      runTerminalCommand(connectString)
+    } else {
+      ns.tprintf(`ERROR: Host ${ns.args[0]} not found.`)
+    }
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function autocomplete(data: ServerData, args: string[]): string[] {
-	return [...data.servers]
+  return [...data.servers]
 }
--- types.d.ts
+++ types.d.ts
@@ -1,19 +1,19 @@
 export interface ServerData {
-	servers: string[];
-	scripts: string[];
-	txts: string[];
-	flags: string[];
+  servers: string[];
+  scripts: string[];
+  txts: string[];
+  flags: string[];
 }
 
 export interface ActionTimes {
-	hack: number;
-	weaken: number;
-	grow: number;
+  hack: number;
+  weaken: number;
+  grow: number;
 }
 
 export interface HackRatios {
-	weakT: number;
-	hackT: number;
-	growT: number;
-	weak2T: number;
+  weakT: number;
+  hackT: number;
+  growT: number;
+  weak2T: number;
 }
