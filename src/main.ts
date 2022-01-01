b595557881f5785454555a6bef857128353e8d41
Cyn <SlyCedix@gmail.com>
2022-01-01 15:54:47 -0500

    remove hacknet from main script

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NS } from '../NetscriptDefinitions'
 
 
-const scripts = ['hwgw.js', 'hacknet.js', 'contracts.js']
+const scripts = ['hwgw.js', 'contracts.js']
 
 export async function main(ns: NS): Promise<void> {
   for(const script of scripts) ns.run(script)
