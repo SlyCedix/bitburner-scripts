48cc936516f51d0007306bb7d4ecd33ce71ab907
Cyn <SlyCedix@gmail.com>
2022-01-03 03:15:25 -0500

    Add more startup scripts to main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NS } from '../NetscriptDefinitions'
 
 
-const scripts = ['hwgw.js', 'contracts.js']
+const scripts = ['hwgw.js', 'contracts.js', 'watcher.js', 'joe.js']
 
 export async function main(ns: NS): Promise<void> {
     for(const script of scripts) ns.run(script)
