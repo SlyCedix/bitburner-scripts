0fe98059b72b2dc787ce81377fb24ac287701bdc
Cyn <SlyCedix@gmail.com>
2022-01-02 21:51:19 -0500

    Add rgraph compat to sleep function

--- src/hwgw/grow.ts
+++ src/hwgw/grow.ts
@@ -2,11 +2,11 @@ import { NS } from '../../NetscriptDefinitions'
 
 export async function main(ns: NS): Promise<void> {
     const sleep = (ms : number) : Promise<void> => {
-        return new Promise(resolve => setTimeout(resolve,ms))
+        const ret : Promise<void> = new Promise(resolve => setTimeout(resolve,ms))
+        ns.print(`sleep: Sleeping for ${Math.floor(ms)} milliseconds`)
+        return ret
     }
 
-    const sleepTime = (ns.args[1] as number) - performance.now() 
-
-    await sleep(sleepTime)
+    await sleep((ns.args[1] as number) - performance.now() )
     await ns.grow(ns.args[0] as string)
 }
