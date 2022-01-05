7bc9fd979e0455771e28f55c82fae8e7e07eabbb
Cyn <SlyCedix@gmail.com>
2022-01-05 18:23:37 -0500

    Add scriptKill to main

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { NS } from '../NetscriptDefinitions'
 const scripts = ['hwgw.js', 'contracts.js', 'watcher.js']
 
 export async function main(ns: NS): Promise<void> {
-    for(const script of scripts) ns.run(script)
+    for(const script of scripts) {
+        ns.scriptKill(script, 'home')   
+        ns.run(script)
+    }
 } 
