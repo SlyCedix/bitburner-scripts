ece68a7c50c75e8667d44776873899207d08358f
SlyCedix <slycedix@gmail.com>
2022-01-10 14:09:06 -0500

    Add null check to hwgw subscripts

--- src/hwgw/weaken.ts
+++ src/hwgw/weaken.ts
@@ -6,7 +6,7 @@ export async function main(ns: NS): Promise<void> {
         ns.print(`sleep: Sleeping for ${Math.floor(ms)} milliseconds`)
         return ret
     }
-
+    if (ns.args[1] == null) return
     await sleep((ns.args[1] as number) - performance.now())
     await ns.weaken(ns.args[0] as string)
 }
