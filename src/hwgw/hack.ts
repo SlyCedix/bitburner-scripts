8a0360b0a81d6c9d3f29d008b79775a09fef05a1
SlyCedix <slycedix@gmail.com>
2022-01-10 18:18:24 -0500

    add err printout when hwgw subscripts us

--- src/hwgw/hack.ts
+++ src/hwgw/hack.ts
@@ -1,12 +1,17 @@
 import { NS } from '@ns'
 
 export async function main(ns: NS): Promise<void> {
+    if (ns.args[1] == null || ns.args[1] == Infinity) {
+        ns.tprintf('ERROR: Got invalid argument')
+        return
+    }
+
     const sleep = (ms: number): Promise<void> => {
         const ret: Promise<void> = new Promise(resolve => setTimeout(resolve, ms))
         ns.print(`sleep: Sleeping for ${Math.floor(ms)} milliseconds`)
         return ret
     }
-    if (ns.args[1] == null) return
+
     await sleep((ns.args[1] as number) - performance.now())
     await ns.hack(ns.args[0] as string)
 }
