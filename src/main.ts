009337cbe741a45d2a28f4cf537a02d99fb469c3
SlyCedix <slycedix@gmail.com>
2022-01-10 18:18:33 -0500

    Add script killing to main

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NS } from '@ns'
 
 
 const scripts = ['hwgw.js', 'contracts.js', 'watcher.js']
+const kill = ['/hwgw/hack.js', '/hwgw/weaken.js', '/hwgw/grow.js']
 
 export async function main(ns: NS): Promise<void> {
+    for (const script of kill) {
+        ns.scriptKill(script, 'home')
+    }
+
     for (const script of scripts) {
         ns.scriptKill(script, 'home')
         ns.run(script)
