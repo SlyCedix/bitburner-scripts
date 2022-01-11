2c41bde37bad489ed82491303b99dd53b8b6511b
SlyCedix <slycedix@gmail.com>
2022-01-11 15:54:43 -0500

    Add profiling logs to hwgw subscripts

--- src/hwgw/hack.ts
+++ src/hwgw/hack.ts
@@ -1,17 +1,17 @@
 import { NS } from '@ns'
 
 export async function main(ns: NS): Promise<void> {
-    if (ns.args[1] == null || ns.args[1] == Infinity) {
-        ns.tprintf('ERROR: Got invalid argument')
-        return
-    }
-
     const sleep = (ms: number): Promise<void> => {
         const ret: Promise<void> = new Promise(resolve => setTimeout(resolve, ms))
         ns.print(`sleep: Sleeping for ${Math.floor(ms)} milliseconds`)
         return ret
     }
 
+    let start = performance.now()
     await sleep((ns.args[1] as number) - performance.now())
+    ns.print(`INFO: Sleep took ${performance.now() - start}`)
+
+    start = performance.now()
     await ns.hack(ns.args[0] as string)
+    ns.print(`INFO: Hack took ${performance.now() - start}`)
 }
